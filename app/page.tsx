acaf7362cb0ac1d920ece2137dc24f5f7b16b843
Carlos Villanueva <carlos2villanueva@gmail.com>
2024-08-27 20:30:34 -0400

    update header

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import ToDo from "@/components/ToDo";
+import ToDo from "@/pages/ToDo";
 
 export default function Home() {
   return (
---
+++ pages/Header.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="sticky top-0 bg-gray-800 text-white">
+        <div className="container">
+          <div className="flex items-center h-16 justify-between">
+            <div className="">LOGO</div>
+            <nav className="hidden md:flex gap-6 items-center">
+              <a href="#" className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700">Create New Item</a>
+              <a href="#" className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700">View Completed</a>
+              <a href="#" className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700">View Pending</a>
+              <a href="#" className="rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700">View Deleted</a>
+            </nav>
+            <div className="">
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center tracking-tight">
+                SignIn
+              </button>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
